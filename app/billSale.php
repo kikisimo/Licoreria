044d316f4b5d52122372afea14e954675ce60b48
wildensx <wildensx@gmail.com>
2017-04-20 03:35:51 -0400

    Segundo seeder Factura_v
    Se agrega el seeder correspondient

--- app/billSale.php
+++ app/billSale.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class billSale extends Model
 {
-    //
+    protected $primaryKey = 'cod_facturaV';
+    protected $foreignKey = 'cod_facturaV';
+    protected $foreignKey2 = 'cod_VP';
 }
