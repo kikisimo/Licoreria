ae349e859c4c568cd0079107fef552bb0a7d1b39
kikisimo <kaikes89@gmail.com>
2017-05-25 15:33:58 -0400

    terminando validacion proveedores

--- resources/views/providers/create_providers.blade.php
+++ resources/views/providers/create_providers.blade.php
@@ -1,5 +1,5 @@
 
-@extends('layout.base0')
+@extends('layout')
 
 @section('content')
     <h1>Registrar Nuevo Proveedor </h1>
@@ -7,26 +7,27 @@
     <div class="col-sm-8">
         <body>
 
+    @include('providers.fragment.error')
+    <div align="center">
 
-<div align="center">
-        <form method="post"action="/licoreria/public/providers/save">
+            <form method="post"action="/licoreria/public/providers/save">
 
-                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <span class="title">NOMBRE DEL PROVEEDOR:  </span><br>
-                <input type="text" id="name" name="name"><br><br>
-                <span class="layout">TELEFONO</span><br>
-                <input type="text" id="phone" name="phone"><br><br>
-                <span class="content">CORREO</span><br>
-                <input type="text" id="e_mail" name="e_mail"><br><br>
-                <span class="content">DIRECCION</span><br>
-                <input type="text" id="address" name="address"><br><br>
-                <span class="content">OBSERVACION</span><br>
-                <input type="text" id="observation" name="observation"><br><br>
-                <input type="submit" value="guardar">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <span class="title">NOMBRE DEL PROVEEDOR:  </span><br>
+                    <input type="text" id="name" name="name"><br><br>
+                    <span class="layout">TELEFONO</span><br>
+                    <input type="text" id="phone" name="phone"><br><br>
+                    <span class="content">CORREO</span><br>
+                    <input type="text" id="e_mail" name="e_mail"><br><br>
+                    <span class="content">DIRECCION</span><br>
+                    <input type="text" id="address" name="address"><br><br>
+                    <span class="content">OBSERVACION</span><br>
+                    <input type="text" id="observation" name="observation"><br><br>
+                    <input type="submit" value="guardar">
 
-        </form>
-</div>
-        </body>
-        <a href="/licoreria/public/base" class="btn btn-primary pull-right"->Pagina Principal</a>
+            </form>
     </div>
-@endsection
+            </body>
+            <a href="/licoreria/public/base" class="btn btn-primary pull-right"->Pagina Principal</a>
+        </div>
+    @endsection
